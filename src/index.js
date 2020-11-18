bf13173b1d52a90ed250b023e166713c79210b2e
MrJeyhun <CeyhunzadeC@kapitalho.az>
2020-11-18 21:25:46 +0400

    drag&drop features was added, some style changes were added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 let taskInputValue = document.getElementById('add-task-input');
+let tasks = document.querySelector('.app__section2__taskcolumn');
+const dropzones = document.querySelectorAll('.dropzone');
 let id = 0;
+let selectedTask;
+
 
 const createTask = (newTask, taskId) => {
     console.log('inputValue', newTask);
@@ -8,8 +12,8 @@ const createTask = (newTask, taskId) => {
     task.className = 'app__section2__added-tasks__task fill';
     task.setAttribute("draggable", "true");
 
-    // task.addEventListener('dragstart', dragStart);
-    // task.addEventListener('dragend', dragEnd);
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('dragend', dragEnd);
 
     let taskContent = document.createElement('div');
     taskContent.className = 'app__section2__added-tasks__task__content';
@@ -23,7 +27,6 @@ const createTask = (newTask, taskId) => {
     task.appendChild(taskContent);
     task.appendChild(trash);
 
-    let tasks = document.querySelector('.app__section2__added-tasks');
     tasks.insertBefore(task, tasks.childNodes[0]);
 }
 
@@ -42,9 +45,44 @@ const removeTask = (id) => {
     document.getElementById(`${id}`).remove();
 }
 
+const dragStart = (event) => {
+    event.target.className += ' hold';
+    selectedTask = event.target;
+    setTimeout(() => (event.target.className = 'invisible'), 0);
+}
+
+const dragEnd = (event) => {
+    event.target.className = 'app__section2__added-tasks__task fill';
+}
+
+const dragEnter = (event) => {
+    event.preventDefault();
+    event.target.className === 'app__section2__taskcolumn dropzone' && (event.target.className += 'hovered');   
+}
+
+const dragOver = (event) => {
+    event.preventDefault();
+}
+
+const dragLeave = (event) => {
+    event.target.className === 'app__section2__taskcolumn dropzone hovered' && (event.target.className = "app__section2__taskcolumn dropzone")
+}
+
+const dragDrop = (event) => {
+    event.target.className === 'app__section2__taskcolumn dropzone hovered' && (event.target.className = "app__section2__taskcolumn dropzone");
+    event.target.append(selectedTask);
+}
 
 // Adding event listeners to 'Add' button
 document.getElementById('add-task').addEventListener('click', addTask);
 
 //Listening add task input
 taskInputValue.addEventListener('change', (event) => taskInputValue = event.target.value);
+
+//Apply drag&drop events to all dropzones
+for ( const dropzone of dropzones ) {
+    dropzone.addEventListener('dragenter', dragEnter);
+    dropzone.addEventListener('dragover', dragOver);
+    dropzone.addEventListener('dragleave', dragLeave);
+    dropzone.addEventListener('drop', dragDrop);
+}
